9f9e53a1569c570d92a38616b7b8f5496780e545
komatsu_home <komatsu@robot.t.u-tokyo.ac.jp>
2017-02-13 12:28:44 +0900

    delete unused files

--- lis/unity-sample-environment/Assets/Scripts/Defs.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-namespace MLPlayer {
-	public class Defs {
-		public static string PLAYER_TAG = "Player";
-		public static string PLAYER = "FPSController";
-	}
-}
